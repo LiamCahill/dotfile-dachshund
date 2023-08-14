068b587f860ce3e4d14560b177d90358fb8f0624
Liam Cahill <cahill.liam220@gmail.com>
2023-08-14 10:51:39 -0400

    add nvim lsp files

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,22 +3,22 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-})
+	'tsserver',
+	'eslint',
+	--'sumneko_lua',
+	'rust_analyzer',
+	})
 
--- Fix Undefined global 'vim'
-lsp.nvim_workspace()
 
-
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm(cmp_select),
+	['<C-Space>'] = cmp.mapping.complete(),
 })
+
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
@@ -57,3 +57,9 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
+
+
+lsp.set_preferences({
+	sign_icons = { }
+})
+
--- nvim/lua/fin/packer.lua
+++ nvim/lua/fin/packer.lua
@@ -1,5 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd.packadd('packer.nvim')
 
@@ -8,12 +6,12 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
-      }
- 
-use({
+  }
+
+  use({
 	  'rose-pine/neovim',
 	  as = 'rose-pine',
 	  config = function()
@@ -21,18 +19,41 @@ use({
 	  end
   })
 
+  use({
+      "folke/trouble.nvim",
+      config = function()
+          require("trouble").setup {
+              icons = false,
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+          }
+      end
+  })
+
+  use {
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,}
+  use("nvim-treesitter/playground")
+  
+  use("theprimeagen/harpoon")
+
+  use("mbbill/undotree")
+
+  use("tpope/vim-fugitive")
+
+
+
 use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
   requires = {
     -- LSP Support
     {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
-    },
+    {'williamboman/mason.nvim'},           -- Optional
     {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
     -- Autocompletion
@@ -43,13 +64,5 @@ use {
 }
 
 
-
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  
-
-use('theprimeagen/harpoon')
-
-
-
 end)
+
